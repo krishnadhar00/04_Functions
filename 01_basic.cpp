e420c90c5efc68e5548c57412eda3f8ffc3f50d1
krishnadhar pandey <krishnadhargzp00@gmail.com>
2024-08-04 16:58:00 +0530

    first commit

---
+++ 01_basic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void greet()
+{
+    cout<<"Hello World"<<endl;
+    cout<<"My name is krishnadhar Pandey"<<endl;
+}
+int main()
+{
+    greet();
+    cout<<"Done"<<endl;
+    greet();
+}
